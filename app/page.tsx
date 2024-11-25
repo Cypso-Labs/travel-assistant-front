1dc8d1a13b7741ad3f54d40bad0ddf5a7e4c07f0
GVIRDilshan <ishanravidu750@gmail.com>
2024-11-25 17:07:34 +0530

    add ItineraryPage_02 and all responsives

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Datepicker } from "flowbite-react";
 import ItineraryPage from "./components/ItineraryPage";
+import ItineraryPage_02 from "./components/ItineraryPage_02";
 
 
 export default function Home() {
   return (
     <div>
       {/* <Datepicker /> */}
-      <ItineraryPage />
+      <ItineraryPage_02 />
     </div>
   );
 }
