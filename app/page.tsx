750c622adee05737b80a31d66387babd4909bd32
tharumadhushan <tharushamadhushan555@gmail.com>
2024-11-25 20:07:53 +0530

    create itinerery second page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import ItineraryPage from "../components/ItineraryPage";
+import ItineraryPage from "./components/ItineraryPage";
+import ItineraryPage2 from "./components/ItineraryPage2";
 
 export default function Home() {
   return (
     <div>
-      <ItineraryPage />
+      {/* <ItineraryPage /> */}
+      <ItineraryPage2/>
     </div>
   );
 }
