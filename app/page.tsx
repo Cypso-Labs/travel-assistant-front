f3fb3ce51880f00abe4792a80af5814dfcef7c92
tharumadhushan <tharushamadhushan555@gmail.com>
2024-11-25 23:46:54 +0530

    create itinerery third page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ItineraryPage from "./components/ItineraryPage";
 import ItineraryPage2 from "./components/ItineraryPage2";
+import ItineraryPage3 from "./components/ItineraryPage3";
 
 export default function Home() {
   return (
     <div>
       {/* <ItineraryPage /> */}
-      <ItineraryPage2/>
+      {/* <ItineraryPage2/> */}
+      <ItineraryPage3/>
     </div>
   );
 }
