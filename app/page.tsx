f822b84af6b8e827555be8926c827a93b475d7f5
GVIRDilshan <ishanravidu750@gmail.com>
2024-11-25 10:52:36 +0530

    add ItineraryPage_01

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Datepicker } from "flowbite-react";
+import ItineraryPage from "./components/ItineraryPage";
 
 
 export default function Home() {
   return (
     <div>
-      <Datepicker />
+      {/* <Datepicker /> */}
+      <ItineraryPage />
     </div>
   );
 }
