e3eb75b824fd1eb9bd2cc1713efd1fbaced62943
anupaWanigasinghe <anupa@cypsolabs.com>
2024-11-27 23:00:00 +0530

    added sign in alerts

--- app/account/sign-in/action.ts
+++ app/account/sign-in/action.ts
@@ -3,6 +3,8 @@
 import { signInSchema } from "@/app/lib/schemas";
 import { parseWithZod } from "@conform-to/zod";
 import axios from "axios";
+import router from "next/router";
+import Swal from "sweetalert2";
 // import { useRouter } from "next/router";
 
 export const logInUser = async (
@@ -29,12 +31,30 @@ export const logInUser = async (
         .then((response) => {
             console.log(response);
 
+            if (response.status === 200) {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Successfully logged in.",
+                    icon: "success"
+                });
+            }
             localStorage.setItem('UserData', JSON.stringify(response.data))
-            // setTimeout(() => {
-            //     router.push('/account/profile-creation');
-            // }, 1500)
+
+            setTimeout(() => {
+                router.push('/account/profile-creation');
+            }, 1500)
         })
         .catch((error) => {
+
+            const { response } = error;
+
+            if (response.status === 401) {
+                Swal.fire({
+                    title: "Error!",
+                    text: "Invalid Email or Password",
+                    icon: "error"
+                });
+            }
             console.log(error);
         });
 };
