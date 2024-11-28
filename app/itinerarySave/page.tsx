83d0fa8870e349aface822317985920f95e4c1c2
GVIRDilshan <ishanravidu750@gmail.com>
2024-11-28 18:29:49 +0530

    first commit

---
+++ app/itinerarySave/page.tsx
@@ -0,0 +1,239 @@
+"use client";
+
+// import React from "react";
+import VR from "../../public/images/icons/VR_Icon.png";
+import header_image from "../../public/images/hedar_Img.png";
+import Map from "../../public/images/Map_IMG.png";
+import Popup from "./Modals/saveItineraryModal";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import VR360Image from "./Modals/vrModal";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export default function ItineraryPage_02() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedImageURL, setSelectedImageURL] = useState("");
+  const [itineraryData, setItineraryData] = useState([]);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [itemData, setItemData] = useState([]);
+
+  const user = JSON.parse(localStorage.getItem('UserData'));
+
+  useEffect(() => {
+
+    let locations = [];
+
+    const fetchAllLocations = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/v1/locations', {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          locations = response.data.locations;
+        }
+      } catch (error) {
+        const { response } = error;
+        console.log(response.data);
+      }
+    };
+
+    const savedData = localStorage.getItem("itinerary");
+    if (savedData) {
+      const parsedData = JSON.parse(savedData);
+
+      // Fetch all locations first
+      fetchAllLocations().then(() => {
+        const transformedData = parsedData.map((item) => ({
+          locations: item.sub_places.map((place, index) => {
+            // Find the matching location from the database
+            const matchedLocation = locations?.find(
+              (dbLocation) => dbLocation.name.toLowerCase() === place.name.toLowerCase()
+            );
+
+
+            // Return the complete object by merging predicted data with database data
+            return {
+              id: index + 1,
+              name: place.name,
+              description: matchedLocation?.description || `Lat: ${place.latitude}, Lon: ${place.longitude}`,
+              latitude: matchedLocation?.latitude || place.latitude,
+              longitude: matchedLocation?.longitude || place.longitude,
+              location_image: matchedLocation?.location_image || "Default_Image_URL", // Use a default if not found
+              budget: `Approx Rs ${item.cost}`,
+              type: matchedLocation?.type || "Unknown",
+              location_id: matchedLocation?.id || "Unknown",
+            };
+          }),
+          totalBudget: item.cost,
+        }));
+
+
+        setItineraryData(transformedData);
+      });
+
+    }
+  }, []);
+
+
+  const openPopup = () => setIsPopupOpen(true);
+  const closePopup = () => setIsPopupOpen(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
+  const handleImageModal = (imageURL: string) => {
+    setSelectedImageURL(imageURL);
+    setOpenModal(true);
+  };
+
+  const clearLocalStorage = () => {
+    localStorage.removeItem("itinerary");
+    setItineraryData([]);
+    Swal.fire({
+      title: "Data Cleared",
+      text: "Itinerary data cleared!",
+      icon: "success"
+    });
+  };
+
+  const handleSaveItinerary = async (item) => {
+    setItemData(item);
+    openPopup();
+  }
+
+
+  const Alert = () => {
+    return (
+      <div id="alert-border-3" className="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800" role="alert">
+        <svg className="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <div className="ms-3 text-sm font-medium">
+          A simple success alert with an <a href="#" className="font-semibold underline hover:no-underline">example link</a>. Give it a click if you like.
+        </div>
+        <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-border-3" aria-label="Close">
+          <span className="sr-only">Dismiss</span>
+          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+          </svg>
+        </button>
+      </div>
+    );
+  }
+
+
+  const handleShowAlert = () => {
+    setShowAlert(true);
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-64 bg-gray-800 text-white transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } transition-transform duration-300 z-50`}
+      >
+        {/* Sidebar content */}
+      </div>
+
+      <main className="container mx-auto mt-6 pb-10 px-4 sm:px-6 lg:px-8">
+        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
+          Your Itinerary
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+          {itineraryData.map((item, index) => (
+            <div
+              key={index}
+              className="bg-white shadow rounded-lg p-4 flex flex-col relative"
+            >
+              <div className="w-full bg-gray-300 mb-4 rounded">
+                <Image
+                  src={Map}
+                  alt="Map"
+                  className="w-full h-full object-cover rounded"
+                />
+              </div>
+              <div className="mb-4">
+                {item.locations.map((loc) => (
+                  <div
+                    key={loc.id}
+                    className="flex items-center justify-between mb-2"
+                  >
+                    <label className="flex items-center text-gray-700 space-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-5 h-5 text-green-300"
+                      >
+                        <circle cx="12" cy="12" r="8" />
+                      </svg>
+                      <span>
+                        {loc.name}{" "}
+                        <span className="text-sm">({loc.description})</span>
+                      </span>
+                    </label>
+                    <span className="text-gray-500">
+                      <Image
+                        onClick={() => {
+                          handleImageModal(loc.location_image);
+                        }}
+                        src={VR}
+                        alt="Location Icon"
+                        className="w-5 h-5 cursor-pointer"
+                      />
+                    </span>
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-col items-center sm:items-start mt-4">
+                <p className="text-gray-700 font-semibold text-center sm:text-left">
+                  Total Budget
+                </p>
+                <p className="text-xl font-bold text-green-600 text-center sm:text-left">
+                  RS {item.totalBudget}
+                </p>
+              </div>
+              <div className="relative mt-4 sm:mt-6 flex justify-end w-full">
+                <button onClick={() => { handleSaveItinerary(item) }} className="bg-green-500 text-white py-2 px-4 w-40 rounded-md hover:bg-green-600 transition">
+                  SAVE
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        {openModal && (
+          <VR360Image
+            imageURL={selectedImageURL}
+            onClose={() => setOpenModal(false)}
+          />
+        )}
+      </main>
+
+
+      <div className="fixed bottom-4 right-4 z-50">
+        <button
+          onClick={clearLocalStorage}
+          className="bg-red-500 text-white px-6 py-2 rounded shadow-md hover:bg-red-600 transition"
+        >
+          Clear Data
+        </button>
+      </div>
+      {isPopupOpen && (
+        <Popup
+          onClose={closePopup}
+          onItemData={itemData}
+          onSuccess={handleShowAlert}
+        />
+      )}
+
+    </div>
+  );
+}
---
+++ app/itineraryView/page.tsx
@@ -0,0 +1,343 @@
+"use client"; // Required for Next.js apps
+
+import React, { useEffect, useState } from "react";
+import header_image from '../../public/images/hedar_Img.png'
+import delete_image from '../../public/images/icons/delete.png'
+import edit_image from '../../public/images/icons/edit.png'
+import file_image from '../../public/images/icons/document.png'
+import VR from '../../public/images/icons/VR_Icon.png'
+import Image from "next/image";
+import axios from "axios";
+import MapComponent from "../components/googleMapItinerary";
+import VR360Image from "../components/Modals/vrModal";
+import Swal from "sweetalert2";
+
+
+export default function ItineraryPage() {
+
+  const user = JSON.parse(localStorage.getItem('UserData') || '{}');
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [itinerary, setItinerary] = useState([]);
+  const [selectedLocations, setSelectedLocations] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedImageURL, setSelectedImageURL] = useState("");
+  const [selectedItinerary, setSelectedItinerary] = useState(null);
+
+
+  useEffect(() => {
+
+    let locations = [];
+
+    const fetchAllItineraries = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/v1/itinerary/${user.user_id}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          locations = response.data.itineraries;
+          console.log(response.data.itineraries);
+          setItinerary(response.data.itineraries); // Store all locations in state
+        }
+      } catch (error) {
+        const { response } = error;
+        console.log(response);
+        if (response.status === 404) {
+          setItinerary(null);
+        }
+      }
+    };
+
+    fetchAllItineraries()
+
+  }, []);
+
+
+  const fetchLocationsForItinerary = async (item) => {
+    // Get user data from localStorage
+    const user = JSON.parse(localStorage.getItem('UserData') || '{}');
+    setSelectedItinerary(item);
+
+    // If user data exists and contains user_id
+    if (user && user.user_id) {
+      try {
+        // Send GET request with itinerary id and user id
+        const response = await axios.get(
+          `http://localhost:5000/api/v1/itinerary_location/${item.id}/${user.user_id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`, // Include the token in headers
+            },
+          }
+        );
+
+        console.log(response);
+
+        // Check if the response status is 200 (OK)
+        if (response.status === 200) {
+          setSelectedLocations(response.data.locations); // Assuming API returns an array of locations
+        }
+      } catch (error) {
+        console.error('Error fetching locations:', error.response?.data || error.message);
+      }
+    } else {
+      console.error('User data not found or missing user_id');
+    }
+  };
+
+  const handleImageModal = (imageURL: string) => {
+    setSelectedImageURL(imageURL);
+    setOpenModal(true);
+  };
+
+
+  const handleDeleteItinerary = async (id) => {
+    try {
+      // Display confirmation alert using SweetAlert
+      const result = await Swal.fire({
+        title: 'Are you sure?',
+        text: "This action will permanently delete the itinerary.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'Cancel',
+      });
+
+      // Check if the user confirmed the action
+      if (result.isConfirmed) {
+        // Send DELETE request to the backend
+        const response = await axios.delete(`http://localhost:5000/api/v1/itineraries/${id}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+          },
+        });
+
+        console.log(response);
+
+        if (response.status === 200) {
+          // Display success message
+          Swal.fire('Deleted!', response.data.message || 'Itinerary has been deleted.', 'success');
+
+          // Update the state to remove the deleted itinerary
+          setItinerary((prevItinerary) => prevItinerary.filter(item => item.id !== id));
+        }
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error('Error deleting itinerary:', error.response.data);
+
+        // Display error message
+        Swal.fire('Error!', error.response.data.message || 'Failed to delete itinerary.', 'error');
+      } else {
+        console.error('Error deleting itinerary:', error.message);
+
+        // Display error message
+        Swal.fire('Error!', 'Something went wrong. Please try again later.', 'error');
+      }
+    }
+  };
+
+
+  // Handle remove itinerary
+  const handleRemove = (id: number) => {
+    setItinerary(itinerary.filter(item => item.id !== id));
+  };
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
+  // const totalBudget = itinerary.reduce((acc, item) => {
+
+  //   const numericBudget = parseInt(item.total_budget);
+  //   return acc + numericBudget;
+  // }, 0);
+
+  // console.log(`Total Budget: RS ${totalBudget}`);
+
+  const handleImageClick = () => {
+    console.log("Image clicked!");
+  };
+
+
+
+  function removeMainSection(): void {
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-64 bg-gray-800 text-white transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } transition-transform duration-300 z-50`}
+      >
+        <div className="flex justify-between items-center p-4 border-b border-gray-700">
+          <h2 className="text-xl font-bold">Menu</h2>
+          <button
+            onClick={toggleSidebar}
+            className="text-white hover:text-gray-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <nav className="p-4 space-y-4">
+          <a href="/" className="block hover:text-gray-300">Home</a>
+          <a href="/itinerary" className="block hover:text-gray-300">Itinerary</a>
+          <a href="/events" className="block hover:text-gray-300">Events</a>
+          <a href="/recipes" className="block hover:text-gray-300">Recipes</a>
+          <a href="/emergency" className="block hover:text-gray-300">Emergency</a>
+          <a href="/about" className="block hover:text-gray-300">About Us</a>
+        </nav>
+      </div>
+
+      {/*back button*/}
+      <div className="bg-white p-6 rounded-lg shadow-lg border flex py-3 mt-6 ml-10 mr-10">
+        <button className=" bg-black text-white px-1 rounded hover:bg-red-600 transition duration-200">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 transform rotate-180">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14M12 5l7 7-7 7" />
+          </svg>
+        </button>
+        <h2 className="text-xl font-bold absolute right-0 px-20">My Itinerary</h2>
+      </div>
+
+
+      {/* Itinerary Table Section */}
+      {itinerary ? <section className="container mx-auto px-4 py-10">
+        <h2 className="text-2xl font-bold mb-6 text-left">My Itinerary</h2>
+        <div className="overflow-x-auto shadow rounded-lg">
+          <table className="min-w-[800px] w-full bg-white text-left border-collapse">
+            <thead className="bg-gray-100 border-b">
+              <tr>
+                <th className="px-6 py-3 text-sm font-medium text-gray-900"></th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-900">Name</th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-900">Budget</th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-900">Start Date</th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-900">End Date</th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-900"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {itinerary.map(item => (
+                <tr key={item.id} className="border-b hover:bg-gray-50" onClick={() => fetchLocationsForItinerary(item)}>
+                  {/* Edit Action */}
+                  <button className="text-blue-500 hover:text-blue-700">
+                    <Image src={file_image} alt="Icon" className="w-6 h-6" />
+                  </button>
+                  <td className="px-6 py-4 text-sm text-gray-700 ">{item.name}</td>
+                  <td className="px-6 py-4 text-sm text-gray-700 ">{item.total_budget}</td>
+                  <td className="px-6 py-4 text-sm text-gray-700 ">{item.start_date}</td>
+                  <td className="px-6 py-4 text-sm text-gray-700 ">{item.end_date}</td>
+                  <td className="px-6 py-4 flex items-center space-x-4">
+                    {/* Remove Action */}
+                    <button
+                      onClick={() => handleDeleteItinerary(item.id)}
+                      className="text-red-500 hover:text-red-700">
+                      <Image src={delete_image} alt="Icon" className="w-6 h-6" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+        :
+        <p className="text-2xl font-semibold text-gray-700 text-center mt-5">No Saved Itineraries</p>
+      }
+
+      <main className="container mx-auto mt-6 pb-10 px-4 sm:px-6 lg:px-8">
+        <h2 className="text-2xl font-semibold text-gray-700 mb-4">Preview</h2>
+
+        {/* Card Section */}
+        {selectedItinerary ? <div className="bg-white shadow-md rounded-lg p-6 w-full">
+
+          {/* Map Section */}
+          <MapComponent locations={selectedLocations} />
+
+          {/* Location and Budget Section */}
+          <div className="flex flex-col lg:flex-row justify-between mb-4">
+            {/* Locations List */}
+            <div className="mb-6 lg:w-2/3">
+              {selectedLocations.map((loc) => (
+                <div
+                  key={loc.id}
+                  className="flex items-center justify-between mb-3 px-2 py-1 bg-gray-50 rounded hover:bg-gray-100 transition"
+                >
+                  <div className="flex items-center space-x-2">
+                    {/* Label */}
+                    <label className="flex items-center text-gray-700 space-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-5 h-5 text-green-400"
+                      >
+                        <circle cx="12" cy="12" r="8" />
+                      </svg>
+                      <span>
+                        {loc.name}{" "}
+                        <span className="text-sm text-gray-500">({loc.description})</span>
+                      </span>
+                    </label>
+
+                    {/* VR Button */}
+                    <button
+                      onClick={handleImageClick}
+                      className="text-gray-500 hover:text-gray-700 flex items-center"
+                    >
+                      <Image
+                        onClick={() => {
+                          handleImageModal(loc.location_image);
+                        }}
+                        src={VR}
+                        alt="VR Icon"
+                        className="w-5 h-5 cursor-pointer"
+                      />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+            {openModal && (
+              <VR360Image
+                imageURL={selectedImageURL}
+                onClose={() => setOpenModal(false)}
+              />
+            )}
+            {/* Budget Section */}
+            <div className="flex flex-col items-center sm:items-start lg:w-1/3 mt-4 lg:mt-0 pt-4">
+              <p className="text-gray-700 font-semibold text-center sm:text-left">
+                Total Budget
+              </p>
+              <p className="text-xl font-bold text-green-600 text-center sm:text-left">
+                RS {selectedItinerary.total_budget}
+              </p>
+            </div>
+          </div>
+        </div>
+          :
+          <p className="text-2xl font-semibold text-gray-500 text-center">Select An Itinerary to preview</p>
+        }
+      </main>
+    </div>
+  );
+}
