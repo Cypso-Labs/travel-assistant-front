5a547c0d7a0350948ab3557e4c1bf53df5e38559
anupaWanigasinghe <anupa@cypsolabs.com>
2024-11-29 19:43:08 +0530

    fixed responsive issues

--- app/(auth)/account/layout.tsx
+++ app/(auth)/account/layout.tsx
@@ -7,12 +7,12 @@ export default function SignUpLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex max-lg:items-center max-lg:justify-center">
-      <div>
-        <Image src={sideImage} alt="sun rise image" />
+    <div className="w-full flex max-lg:items-center max-lg:justify-center h-screen">
+      <div className="w-full lg:w-1/2 h-screen">
+        <Image src={sideImage} alt="sun rise image" className="min-h-screen w-full h-screen" />
       </div>
 
-      <div className="flex-grow max-lg:absolute max-lg:w-full">{children}</div>
+      <div className="flex-grow max-lg:absolute max-lg:w-full max-h-screen overflow-y-auto">{children}</div>
     </div>
   );
 }
