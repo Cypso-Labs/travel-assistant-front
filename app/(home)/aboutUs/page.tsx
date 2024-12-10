1840e78f643152e22e4bb68752321361ef17be1f
Nadula494 <nadula@cypsolabs.com>
2024-12-10 16:39:48 +0530

    created about page in initiale stage

--- app/(home)/aboutUs/page.tsx
+++ app/(home)/aboutUs/page.tsx
@@ -1,13 +1,23 @@
-// /aboutUs/page.js
-const AboutUs = () => {
+"use client";
 
-    
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
+
+export default function  AboutUs() {
+
+
+  ;
+
+
+
+
+
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Welcome to the About Us page!</p>
+    <div className="min-h-screen bg-gray-50  pt-24">
+    <h1>Hi</h1>
     </div>
   );
-};
-
-export default AboutUs;
+}
