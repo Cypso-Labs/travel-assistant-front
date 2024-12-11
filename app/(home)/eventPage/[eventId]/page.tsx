4edd6d69893d6cfb9a55c19b8d7fe02229b67fe9
Dulmika <dulmika@cypsolabs.com>
2024-12-11 18:11:38 +0530

    Connect with 'Event Get By Id' API

--- app/(home)/eventPage/[eventId]/page.tsx
+++ app/(home)/eventPage/[eventId]/page.tsx
@@ -1,47 +1,80 @@
-import React from "react";
-import Image from "next/image";
+"use client";
 
-const page = () => {
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import axios, { AxiosResponse } from "axios";
+import { usePathname } from "next/navigation";
+
+const Page = () => {
+  const eventId = usePathname().split("/").pop();
+
+  const [event, setEvent] = useState<{
+    cover_image: string;
+    description: string;
+    end_date: Date;
+    id: number;
+    name: string;
+    start_date: Date;
+    type: string;
+  }>({
+    cover_image: "",
+    description: "",
+    end_date: new Date(),
+    id: 0,
+    name: "",
+    start_date: new Date(),
+    type: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/v1/events/${eventId}`)
+      .then(
+        (
+          response: AxiosResponse<{
+            code: number;
+            event: {
+              cover_image: string;
+              description: string;
+              end_date: Date;
+              id: number;
+              name: string;
+              start_date: Date;
+              type: string;
+            };
+          }>
+        ) => {
+          console.log(response);
+          setEvent(response.data.event);
+        }
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [eventId]);
+
   return (
     <div className="w-full h-full fixed top-0 left-0 pt-24">
       <div className="relative w-full h-full flex justify-center items-center">
         <div className="bg-black opacity-40 w-full h-full absolute left-0 top-0"></div>
         <div className="relative w-1/2 aspect-[897/698] bg-white rounded-xl min-w-[960px]">
           <div className="relative w-full h-[55%] flex-grow rounded-t-xl bg-black">
-            <Image
-              src="/images/2.png"
-              alt=""
-              layout="fill"
-              className="w-full h-full object-cover rounded-xl"
-            />
+            {event.cover_image ?? (
+              <Image
+                src={event.cover_image}
+                alt=""
+                layout="fill"
+                className="w-full h-full object-cover rounded-xl"
+              />
+            )}
           </div>
           <div className="absolute w-full h-[50%] bg-white top-[50%] left-0 rounded-xl p-6">
-            <h1 className="text-[25px] font-bold">
-              Poson-Poya Thorana (Religon)
-            </h1>
+            <h1 className="text-[25px] font-bold">{event?.name}</h1>
             <h1 className="text-[16px] font-bold pb-6">
-              2025/06/1 - 2025/06/01
+              {event.start_date.toDateString()} -{" "}
+              {event.end_date.toDateString()}
             </h1>
-            <p className="text-sm font-normal">
-              <span className="relative mb-4 block">
-                Poson Poya Thorana are magnificent illuminated displays that are
-                a key highlight of the Poson Poya festival in Sri Lanka.
-                Celebrated in June, Poson Poya commemorates the introduction of
-                Buddhism to the island by Arahant Mahinda. Thoranas are large,
-                decorative panels adorned with vibrant lights, intricate
-                artwork, and vivid storytelling. They often depict significant
-                scenes from Buddhist teachings, such as the life of the Buddha,
-                Jataka tales, or the story of Arahant Mahinda’s arrival.
-              </span>
-              <span className="relative">
-                These stunning structures are crafted with immense creativity
-                and dedication, becoming a focal point in towns and villages
-                during the festival. Poson Poya Thoranas not only add color and
-                splendor to the celebrations but also serve as a medium to
-                convey moral and spiritual lessons, bringing communities
-                together in reflection and festivity.
-              </span>
-            </p>
+            <p className="text-sm font-normal">{event.description}</p>
           </div>
         </div>
       </div>
@@ -49,4 +82,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- app/(home)/eventPage/page.tsx
+++ app/(home)/eventPage/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       .catch((error) => {
         console.log(error);
       });
-  });
+  }, []);
 
   return (
     <div>
