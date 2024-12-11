d143f2fc7e657b761e8196708d4fdc0ecff8b720
Nadula494 <nadula@cypsolabs.com>
2024-12-11 18:06:02 +0530

    buid error fixed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost"], // Add the domain of the image source
+    domains: ["localhost"], 
   },
   logging: false,
 };
