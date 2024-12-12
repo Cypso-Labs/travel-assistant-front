76b16d89ffedb7c9569c086a49ee83129664816a
Dulmika <dulmika@cypsolabs.com>
2024-12-12 12:26:16 +0530

    Create a dropdown and connect 'Location Get All' API

--- app/(home)/eventPage/page.tsx
+++ app/(home)/eventPage/page.tsx
@@ -4,6 +4,17 @@ import Header from "@/app/components/Header";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import axios, { AxiosResponse } from "axios";
+import { Dropdown } from "flowbite-react";
+
+interface Location {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  location_image: string;
+  description: string;
+  type: string;
+}
 
 export default function Home() {
   const [eventList, setEventlist] = useState(
@@ -45,12 +56,38 @@ export default function Home() {
       });
   }, []);
 
+  const [locations, setLocations] = useState(Array<Location>);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/v1/locations")
+      .then((response) => {
+        console.log(response);
+        setLocations(response.data.locations);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
       <main className="bg-white">
         <section className="container mx-auto py-10  pt-40">
-          <h2 className="text-5xl font-bold mb-6 pb-20">Events nearby you</h2>
+          <div className="mx-6 flex justify-between">
+            <h2 className="text-5xl font-bold mb-6 pb-20">Events nearby you</h2>
+
+            <div>
+              <div>
+                <Dropdown label="Dropdown button on" dismissOnClick={false}>
+                  {locations.map((location, index) => (
+                    <Dropdown.Item key={index}>{location.name}</Dropdown.Item>
+                  ))}
+                </Dropdown>
+              </div>
+            </div>
+          </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-40">
             {/* Card 1 */}
@@ -89,85 +126,6 @@ export default function Home() {
                 </div>
               );
             })}
-
-            {/* Repeat for other cards */}
-            {/* Card 2 */}
-            {/* <div className="flex w-[380px] h-[350px] bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden items-start">
-              <div className="w-2/3 p-4">
-                <h3 className="text-lg font-bold text-gray-800 pb-8">
-                  Poson-Poya Thorana (Religion)
-                </h3>
-                <p className="mt-2 text-sm text-gray-600 pb-20">
-                  Poson-Poya Thorana features beautifully illuminated displays
-                  created during the Poson-Poya festival in Sri Lanka.
-                </p>
-                <a
-                  href="https://en.wikipedia.org/wiki/Poson"
-                  className="block mt-4 bg-green-500 text-white text-center py-2 px-4 rounded-md hover:bg-green-600"
-                >
-                  Learn More...
-                </a>
-              </div>
-              <div className="w-1/3 h-full">
-                <Image
-                  src={Torana_Image}
-                  alt="Event Image"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            </div> */}
-
-            {/* Card 3
-            <div className="flex w-[380px] h-[350px] bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden items-start">
-              <div className="w-2/3 p-4">
-                <h3 className="text-lg font-bold text-gray-800 pb-8">
-                  Poson-Poya Thorana (Religion)
-                </h3>
-                <p className="mt-2 text-sm text-gray-600 pb-20">
-                  Poson-Poya Thorana features beautifully illuminated displays
-                  created during the Poson-Poya festival in Sri Lanka.
-                </p>
-                <a
-                  href="https://en.wikipedia.org/wiki/Poson"
-                  className="block mt-4 bg-green-500 text-white text-center py-2 px-4 rounded-md hover:bg-green-600"
-                >
-                  Learn More...
-                </a>
-              </div>
-              <div className="w-1/3 h-full">
-                <Image
-                  src={Torana_Image}
-                  alt="Event Image"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            </div> */}
-
-            {/* Card 4
-            <div className="flex w-[380px] h-[350px] bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden items-start">
-              <div className="w-2/3 p-4">
-                <h3 className="text-lg font-bold text-gray-800 pb-8 ">
-                  Poson-Poya Thorana (Religion)
-                </h3>
-                <p className="mt-2 text-sm text-gray-600 pb-20">
-                  Poson-Poya Thorana features beautifully illuminated displays
-                  created during the Poson-Poya festival in Sri Lanka.
-                </p>
-                <a
-                  href="https://en.wikipedia.org/wiki/Poson"
-                  className="block mt-4 bg-green-500 text-white text-center py-2 px-4 rounded-md hover:bg-green-600"
-                >
-                  Learn More...
-                </a>
-              </div>
-              <div className="w-1/3 h-full">
-                <Image
-                  src={Torana_Image}
-                  alt="Event Image"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            </div> */}
           </div>
         </section>
       </main>
