6684fea3cc158d5b542c84471b21088dbf35bae0
Nadula494 <nadula@cypsolabs.com>
2024-12-13 17:02:15 +0530

    page deleted

--- app/(home)/aboutUs/HighlightedText.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { HighlightedTextProps } from "./types";
-
-export const HighlightedText: React.FC<HighlightedTextProps> = ({
-  text,
-  color,
-}) => {
-  return <span style={{ color }}>{text}</span>;
-};
